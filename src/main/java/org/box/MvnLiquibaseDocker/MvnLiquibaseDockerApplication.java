a7c20ca2ec937142deeeea5327f0ae9776576bac
box <lifesphere15@gmail.com>
2022-04-20 10:26:32 +1000

    init

---
+++ src/main/java/org/box/MvnLiquibaseDocker/MvnLiquibaseDockerApplication.java
@@ -0,0 +1,13 @@
+package org.box.MvnLiquibaseDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnLiquibaseDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnLiquibaseDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/box/MvnLiquibaseDocker/MvnLiquibaseDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.box.MvnLiquibaseDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnLiquibaseDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
